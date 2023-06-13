9e3997f2dc5b10b7bc74a4ca2ed07ee3103381f1
Diego Villafuerte <diego.villafuerte.soraiz@gmail.com>
2023-06-13 12:15:38 +0200

    cualquier cosa

--- modules/dbOperations.py
+++ modules/dbOperations.py
@@ -1,57 +1,52 @@
-import os
-import psycopg2
+from sqlalchemy.orm import sessionmaker
+from dbCreation import Meal, engine
+
+Session = sessionmaker(bind=engine)
 
 def create_connection():
-    conn = psycopg2.connect(os.environ["DATABASE_URL"])
-    return conn
+    return Session()
 
-def close_connection(conn):
-    conn.close()
+def close_connection(session):
+    session.close()
 
-def create_meal_table(conn):
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS meals (
-                        id SERIAL PRIMARY KEY,
-                        meal TEXT NOT NULL,
-                        calories INTEGER NOT NULL,
-                        carbohydrates INTEGER,
-                        protein INTEGER,
-                        fat INTEGER,
-                        sodium INTEGER,
-                        date TIMESTAMP NOT NULL)''')
-    conn.commit()
+def add_meal(session, meal):
+    new_meal = Meal(
+        meal=meal['meal'],
+        calories=meal['calories'],
+        carbohydrates=meal['carbohydrates'],
+        protein=meal['protein'],
+        fat=meal['fat'],
+        sodium=meal['sodium'],
+        date=meal['date']
+    )
+    session.add(new_meal)
+    session.commit()
+    return new_meal.id
 
-def add_meal(conn, meal):
-    cursor = conn.cursor()
-    cursor.execute('''INSERT INTO meals (meal, calories, carbohydrates, protein, fat, sodium, date) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id''', (meal['meal'], meal['calories'], meal['carbohydrates'], meal['protein'], meal['fat'], meal['sodium'], meal['date']))
-    meal_id = cursor.fetchone()[0]
-    conn.commit()
-    return meal_id
+def update_meal(session, meal):
+    session.query(Meal).filter_by(id=meal['id']).update({
+        'meal': meal['meal'],
+        'calories': meal['calories'],
+        'carbohydrates': meal['carbohydrates'],
+        'protein': meal['protein'],
+        'fat': meal['fat'],
+        'sodium': meal['sodium'],
+        'date': meal['date']
+    })
+    session.commit()
 
-def update_meal(conn, meal):
-    cursor = conn.cursor()
-    cursor.execute('''UPDATE meals SET meal = %s, calories = %s, carbohydrates = %s, protein = %s, fat = %s, sodium = %s, date = %s WHERE id = %s''', (meal['meal'], meal['calories'], meal['carbohydrates'], meal['protein'], meal['fat'], meal['sodium'], meal['date'], meal['id']))
-    conn.commit()
+def get_meals(session):
+    return session.query(Meal).all()
 
-def get_meals(conn):
-    cursor = conn.cursor()
-    cursor.execute('''SELECT * FROM meals''')
-    return cursor.fetchall()
+def get_meal(session, meal_id):
+    return session.query(Meal).filter_by(id=meal_id).first()
 
-def get_meal(conn, meal_id):
-    cursor = conn.cursor()
-    cursor.execute('''SELECT * FROM meals WHERE id = %s''', (meal_id,))
-    return cursor.fetchone()
-
-def get_daily_meal_summary(conn, date_str):
-    cursor = conn.cursor()
-    cursor.execute('''SELECT SUM(calories) as total_calories,
-                             SUM(carbohydrates) as total_carbohydrates,
-                             SUM(protein) as total_protein,
-                             SUM(fat) as total_fat,
-                             SUM(sodium) as total_sodium
-                      FROM meals
-                      WHERE date("date") = %s''', (date_str,))
-
-    return cursor.fetchone()
-
+def get_daily_meal_summary(session, date_str):
+    result = session.query(
+        func.sum(Meal.calories).label('total_calories'),
+        func.sum(Meal.carbohydrates).label('total_carbohydrates'),
+        func.sum(Meal.protein).label('total_protein'),
+        func.sum(Meal.fat).label('total_fat'),
+        func.sum(Meal.sodium).label('total_sodium')
+    ).filter(func.date(Meal.date) == date_str).first()
+    return result
