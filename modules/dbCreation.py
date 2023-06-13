c37b9e62f1c3bbaf1b9caaabab7b41df37955864
Diego Villafuerte <diego.villafuerte.soraiz@gmail.com>
2023-06-13 12:12:49 +0200

    adding user management

--- modules/dbCreation.py
+++ modules/dbCreation.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 DATABASE_URL = os.environ['DATABASE_URL']
-print(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
