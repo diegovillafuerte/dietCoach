5951f3a4c69c2c3761e4665607c6b2022c9e6a32
Diego Villafuerte <diego.villafuerte.soraiz@gmail.com>
2023-06-14 04:37:12 +0200

    db correction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from modules.app import app
 
 if __name__ == "__main__":
+    print('hola')
     app.run(debug=True)
--- modules/dbOperations.py
+++ modules/dbOperations.py
@@ -6,6 +6,8 @@ from datetime import date
 
 
 DATABASE_URL = os.environ['DATABASE_URL']
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 engine = create_engine(DATABASE_URL)
 DBSession = sessionmaker(bind=engine)
 Base = declarative_base()
